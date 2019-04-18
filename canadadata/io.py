47a9f50a3d64f134d68773ac944ff6f374e26da9
Dwight Gunning <dgunning@gmail.com>
2019-04-17 21:28:28 -0400

    Add a dedicated io file to contain io methods

--- canadadata/io.py
+++ canadadata/io.py
@@ -1,11 +1,27 @@
 import requests, zipfile, io
 import os
-from functools import lru_cache
 
 
-@lru_cache(maxsize=32)
 def unzip_data(zip_url: str, path='.'):
     response = requests.get(zip_url)
     zip_file = zipfile.ZipFile(io.BytesIO(response.content))
     zip_file.extractall(path=path)
     return tuple([os.path.join(path, f) for f in zip_file.namelist()])
+
+
+def get_filename_from_url(path:str):
+    """
+    Get filename from path
+    """
+    return path.split('/')[-1]
+
+
+def download_file(url: str, path='.'):
+    response = requests.get(url)
+    filename = get_filename_from_url(url)
+    if path:
+        filename = os.path.join(path, filename)
+    with open(filename, 'wb') as fd:
+        for chunk in response.iter_content(chunk_size=128):
+            fd.write(chunk)
+    return filename
