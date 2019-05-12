5413f7b9ad6eb4f19e22885943f5dac83df885ac
Tomi <tomi.adegbite11@gmail.com>
2019-05-11 21:14:16 -0400

    Restart for master

--- notebooks/commonimports.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-from opencanada.statscan import to_wide_format, read_statscan_csv, StatscanZip
-from opencanada.io import unzip_data
--- notebooks/displayoptions.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-pd.set_option('display.max_colwidth', 200)
-pd.set_option('display.max_columns', 400)
-pd.set_option('display.max_rows', 500)
