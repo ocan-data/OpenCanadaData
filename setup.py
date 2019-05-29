6d04483ab8459fb2b51b7c9ca42d9dc0cebb2a2f
Dwight Gunning <dgunning@gmail.com>
2019-05-29 11:01:03 -0400

    Updated setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='canadadata',
+    version='0.1.0',
+    description='Open Canada data',
+    long_description=readme,
+    author='Dwight Gunning',
+    author_email='dgunning@gmail.com',
+    url='https://github.com/ocan-data/OpenCanadaData',
+    license=license,
+    packages=['opencanada']
+)
